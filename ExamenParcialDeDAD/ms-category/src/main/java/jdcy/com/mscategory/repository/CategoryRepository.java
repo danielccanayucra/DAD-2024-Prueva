34b18449480656c6b4fed785042fe3c02e7c0c4a
DanielCY <90200686+danielccanayucra@users.noreply.github.com>
2024-12-02 08:58:29 -0500

    Delete ExamenParcialDeDAD directory

--- ExamenParcialDeDAD/ms-category/src/main/java/jdcy/com/mscategory/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package jdcy.com.mscategory.repository;
-
-import jdcy.com.mscategory.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
